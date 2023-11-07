0790adfd82da80638d425b514a251853dd52860e
Idris TAKERBOUST <idrista@icloud.com>
2023-11-07 20:21:45 +0100

    create channel_picker command

--- lib/discord_watch.rb
+++ lib/discord_watch.rb
@@ -10,18 +10,9 @@ end
 
 bot = Discordrb::Bot.new token: ENV["DISCORD_TOKEN"]
 
-bot.message do |event|
-  if event.message.content == '!channel'
+bot.register_application_command(:channel_picker, 'Setup notification channel') do |cmd|
+  cmd.channel('channel', 'Select a channel to keep in touch about Ruby\'s things', types: [:text], required: true)
 
-    event.channel.send_message(
-      'Select a channel to keep in touch about Ruby\'s things' , false, nil, nil, nil, nil,
-      Discordrb::Components::View.new do |builder|
-        builder.row do |row|
-          row.channel_select(custom_id: 'channel_choice', placeholder: 'Choose one channel', channel_types: [:text])
-        end
-      end
-    )
-  end
 end
 
 bot.channel_select do |event|
@@ -30,6 +21,21 @@ bot.channel_select do |event|
     content: "**[CHANNEL_SELECT]**\nYou have chosen channel: **#{user_choice.name}**",
     ephemeral: true
   )
+bot.application_command(:channel_picker) do |event|
+  server_id = event.server.id
+  channel_id = event.options["channel"]
+  puts "[USER INPUT] channel_picker() { server_id: #{server_id}, channel_id: #{channel_id}}"
+
+  channel = Channel.find_or_initialize_by(server_id: server_id)
+  channel.id = channel_id
+
+  embed = if channel.save
+            { description: ":white_check_mark: Your channel is saved
+            :partying_face: You will stay in informed of Ruby's things" }
+          else
+            { description: ":cry: An error has occurred, your choice hasn't been saved" }
+          end
+  event.respond(embeds: [embed], ephemeral:true)
 end
 
 bot.run
